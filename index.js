40c3331e380c7b7a5a069a871e7b25db136c8867
ZanaGuda <62018613+ZanaGuda@users.noreply.github.com>
2021-12-28 19:20:52 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+ function fun() {  
+  
+  
+alert ("Please contact this number: 038992880"); 
+}
+
+var viti2019 = 212223;
+var viti2020 = 122999;
+var total= viti2019 + viti2020;
+
+var number = "The total number is aproximately: " + total + " visitors";
+document.getElementById("visitors").innerHTML= number;
+
